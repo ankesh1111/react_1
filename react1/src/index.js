f7547682767d5472f18e33d38b8a7c033a98fc4d
ankesh1111 <92791586+ankesh1111@users.noreply.github.com>
2022-05-08 08:29:23 +0530

    assignment

---
+++ react1/src/index.js
@@ -0,0 +1,8 @@
+
+import  { navbar}  from '../components/navbar';
+//console.log('navbar:', navbar);
+import'../styles/navbar.css';
+
+let navbar_div = document.getElementById('navbar');
+
+navbar_div.innerHTML = navbar();
